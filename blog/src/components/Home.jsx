e9c33b773e67ab23bb5a396ed473b3433fb32cfc
nedmorgan <nmorgan1987@gmail.com>
2020-03-22 16:39:34 -0400

    added pictures to home screen

--- blog/src/components/Home.jsx
+++ blog/src/components/Home.jsx
@@ -1,7 +1,45 @@
 import React, { Component } from 'react'
+import { HomeContainer } from '../styles/HomeStyles'
+import Carousel from 'react-bootstrap/Carousel'
 
 export default class Home extends Component {
   render() {
-    return <div>Test</div>
+    return (
+      <HomeContainer>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className='d-block w-50'
+              src='./thailand.jpg'
+              alt='thailand boats'
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className='d-block w-50'
+              src='./elephant.jpg'
+              alt='elephants'
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className='d-block w-50'
+              src='./laos.jpg'
+              alt='monk runnings next to temple'
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className='d-block w-50'
+              src='./myanmar.jpg'
+              alt='three boats in myanmar river'
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className='d-block w-50' src='./rhino.jpg' alt='two rhinos' />
+          </Carousel.Item>
+        </Carousel>
+      </HomeContainer>
+    )
   }
 }
---
+++ blog/src/styles/HomeStyles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  img {
+    margin: auto;
+    margin-top: 2em;
+  }
+`
